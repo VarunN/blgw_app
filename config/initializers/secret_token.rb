55d72135c7d1bc9cb5503a35b5561bd940804322
Varun Nagaraja <varun.nagaraja@gmail.com>
2012-05-26 01:20:13 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlgwApp::Application.config.secret_token = 'b3cdd18204e12f8b573a8afa6b7d8592f9003e9f7d241557be53f790866a707282fd5fa86c4ecfc701ff6d139ae2654f47e81ceef966684c9947d9194ec9a105'
